d7e1b02f3f8d502aed5a452818c148287f52c700
Mo-Meshrif <m.meshrif77@gmail.com>
2024-10-02 21:57:12 +0300

    re-config twitter logic after Oauth 2.0

--- lib/app/utils/constants_manager.dart
+++ lib/app/utils/constants_manager.dart
@@ -5,10 +5,10 @@ class AppConstants {
   static const Locale arabic = Locale('ar');
   static const Locale english = Locale('en');
   static const String langPath = 'assets/translations';
-  static const String twitterApiKey = 'hvKiIs7bNGehX7oWTB71HWn1e';
+  static const String twitterApiKey = 'zAnvNuJ6DAEugaDyknhzOQFkT';
   static const String twitterApiSecretKey =
-      'sPLTtdmXy3G3rh03DHm8m1g8HTBH2ItwGP3afqTK3y7wfGtfWo';
-  static const String twitterRedirectURI = 'flutter-twitter-login://';
+      'iLXLStnSPQj7ITU2aAFk1Vw5sIOzOywuLwdZAZjp0ZVAO9Q3BG';
+  static const String twitterRedirectURI = 'twittersdk://';
   static const String googleScope = 'email';
   static const String usersCollection = 'Users';
   static const String userIdFeild = 'id';
